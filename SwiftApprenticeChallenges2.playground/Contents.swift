dea5733501a6e4b9ba99788dd2c179692e9a76ca
Celia Li <leeonjel@gmail.com>
2016-12-22 11:54:50 +0800

    access control, custom operators, pattern matching

--- SwiftApprenticeChallenges2.playground/Contents.swift
+++ SwiftApprenticeChallenges2.playground/Contents.swift
@@ -211,6 +211,264 @@ func swapped<T, U>(_ x: T, _ y: U) -> (U, T) {
     return (y, x)
 }
 
+/* Access Control
+ private - accessible only to the defining type
+ fileprivate - accessible from anywhere within the source file in which it's defined
+ internal - default, accessible from anywhere within the module in which it's defined
+ public - internal + other modules import this module
+ open - public + can ben overridden from within another module
+ */
+protocol Account {
+    associatedtype Currency
+    
+    var balance: Currency { get }
+    func deposit(amount: Currency)
+    func withdraw(amount: Currency)
+}
+
+typealias Dollars = Double
+
+class BasicAccount: Account {
+    private(set) var balance: Dollars = 0.0
+    
+    func deposit(amount: Dollars) {
+        balance += amount
+    }
+    func withdraw(amount: Double) {
+        if balance > amount {
+            balance -= amount
+        }else {
+            balance = 0
+        }
+    }
+}
+
+//Singleton
+class Singleton {
+    static let sharedInstance = Singleton()
+    init() {
+        print("This is a singleton")
+    }
+    func log(){
+        print("Log log singleton instance log method called")
+    }
+}
+let singleton = Singleton.sharedInstance
+singleton.log()
+
+//Implement a stack
+struct Stack<T> {
+    private(set) var elements: [T] = []
+    
+    func peek() -> T? {
+        return elements.last
+    }
+    mutating func push(_ element: T) {
+        elements.append(element)
+    }
+    mutating func pop() -> T? {
+        if elements.isEmpty {
+            return nil
+        }
+        return elements.removeLast()
+    }
+    func count() -> Int {
+        return elements.count
+    }
+}
+
+//Custom operators
+infix operator **
+func **(lhs: Int, rhs: Int) -> Int {
+    var result = lhs
+    for _ in 2...rhs{
+        result *= lhs
+    }
+    return result
+}
+let base = 2
+let exponent = 3
+let expo1 = base ** exponent
+
+infix operator **=
+func **=(lhs: inout Int, rhs: Int){
+    lhs = lhs ** rhs
+}
+
+var number = 2
+number **= exponent
+
+infix operator ***
+func ***<T: FloatingPoint>(lhs: T, rhs: Int) -> T {
+    var result = lhs
+    for _ in 2...rhs {
+        result *= lhs
+    }
+    return result
+}
+
+let expo2 = Double(base) *** exponent
+let expo3 = 3.45 *** exponent
+
+infix operator ***=
+func ***=<T: FloatingPoint>(lhs: inout T, rhs: Int){
+    lhs = lhs *** rhs
+}
+
+var expo4: Double = 2.5
+expo4 ***= 2
+
+/*
+ pattern matching - if case A = B, guard case A = B
+ wildcard - if case (_, 0, 0) = coordinate { } else { }
+ value binding - if case (let x, 0, 0) = coordinate { }
+ bind multiple values - if case let (x, y, 0) = coordinate { }
+ 
+ */
+//pattern matching using switch statement
+func estimatePopulationSize(population: Int) -> String {
+    let single = 1
+    let aFew = 2...4
+    let several = 5...10
+    let many = 10...50
+    switch population {
+    case single:
+        return "one person"
+    case aFew:
+        return "a few people"
+    case several:
+        return "several people"
+    case many:
+        return "many people"
+    default:
+        return "oh so many people"
+    }
+}
+print("There are \(estimatePopulationSize(population: 35))")
+
+//use pattern matching to count occurance
+let groupSizes = [1, 5, 4, 6, 2, 1, 3]
+for case 1 in groupSizes {
+    print("Found an individual")
+}
+
+let names: [String?] = ["Michelle", nil, "Brandon", "Christine", nil, "David"]
+for name in names {
+    switch name {
+    case .none:
+        break
+    case .some(let name):
+        print("switch pattern matching ound someone who's name is \(name)")
+    }
+}
+
+//Another solution
+for case let name? in names{
+    print("optional pattern found someone who's name is \(name)")
+}
+
+//as, is
+let array: [Any] = [15, "George", 2.0]
+
+for element in array {
+    switch element {
+    case is String:
+        print("Found a string")
+    case is Int:
+        print("Found an integer")
+    default:
+        break
+    }
+}
+
+for element in array {
+    switch element {
+    case let text as String:
+        print("Found a string: \(text)")
+    default:
+        print("Found something else")
+    }
+}
+
+for number in 1...9 {
+    switch number {
+    case let x where x % 2 == 0:
+        print("even")
+    default:
+        print("odd")
+    }
+}
+
+//calculate fibonacci
+func fibonacci(position: Int) -> Int{
+    switch position {
+    case let n where n <= 1:
+        return 0
+    case 2:
+        return 1
+    case let n:
+        return fibonacci(position: n - 1) + fibonacci(position: n - 2)
+    }
+}
+let fib15 = fibonacci(position: 15)
+
+//print fizzbuzz
+for i in 1...20{
+    switch (i % 3, i % 5) {
+    case (0, 0):
+        print("FizzBuzz", terminator: " ")
+    case (0, _):
+        print("Fizz", terminator: " ")
+    case (_, 0):
+        print("Buzz", terminator: " ")
+    case (_, _):
+        print(i, terminator: " ")
+    }
+}
+
+enum FormField {
+    case firstName(String)
+    case lastName(String)
+    case emailAddress(String)
+    case age(Int)
+}
+let minimumAge = 21
+let submittedAge = FormField.age(22)
+
+if case .age(let years) = submittedAge, years > minimumAge {
+    print("Welcome!")
+} else {
+    print("Sorry, you are too young!")
+}
+
+enum CelestialBody {
+    case star
+    case planet(liquidWater: Bool)
+    case comet
+}
+
+let telescopeCensus  = [CelestialBody.star, CelestialBody.planet(liquidWater: false), CelestialBody.planet(liquidWater: true), CelestialBody.planet(liquidWater: true), CelestialBody.comet]
+
+for case .planet(let water) in telescopeCensus where water {
+    print("Found a potentially habitable planet!")
+}
+
+let queenAlbums = [("A night at the opera", 1974), ("Sheer Heart Attack", 1974), ("Jazz", 1978), ("The Game", 1980)]
+for case (let album, 1974) in queenAlbums {
+    print("Found an 1974 album named \(album)")
+}
+
+let coordinates = (lat: 192.89432, long: -68.33294)
+switch coordinates {
+case (let lat, _) where lat < 0:
+    print("In the Northern hemisphere!")
+case (let lat, _) where lat == 0:
+    print("It's on the equator!")
+case (let lat, _) where lat > 0:
+    print("In the Southern hemesphere!")
+default:
+    break
+}
 
 
 
