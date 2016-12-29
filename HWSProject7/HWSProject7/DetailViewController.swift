292e0e5e6ebc19df40a7a259f935382eb95eb04e
Celia Li <leeonjel@gmail.com>
2016-12-29 22:21:31 +0800

    HWS project7 done

---
+++ HWSProject7/HWSProject7/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  HWSProject7
+//
+//  Created by celia  on 29/12/2016.
+//  Copyright © 2016 celia . All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard detailItem != nil else {
+            return
+        }
+        if let body = detailItem["body"]{
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
