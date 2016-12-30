99b738802392cac036834cd46533c63428df113c
Celia Li <leeonjel@gmail.com>
2016-12-30 23:36:59 +0800

    HWS project 9 + 10

---
+++ HWSProject10/HWSProject10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  HWSProject10
+//
+//  Created by celia  on 30/12/2016.
+//  Copyright © 2016 celia . All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
--- HWSProject7 + 9/HWSProject7/ViewController.swift
+++ HWSProject7 + 9/HWSProject7/ViewController.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2016 celia . All rights reserved.
 //
 
+/*Project 9 GCD Notes
+ QoS quality of service 4 background queues that you can use
+ 1.user interactive - highest priority, all available CPU
+ 2.user initiated - tasks requested by user, keeps user waiting
+ 3.utility - long-running tasks that the user is aware of but not desperate for now
+ 4.background - long-running tasks user isn't aware of or care
+ 
+ default queue - general purpose
+ */
+
 import UIKit
 
 class ViewController: UITableViewController {
@@ -14,25 +24,30 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+
+    func fetchJSON() {
         let urlString: String
+        
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         }else {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                let json = JSON(data: data)
-                
-                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                    parse(json: json)
-                    return
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = JSON(data: data)
+                    
+                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                        self.parse(json: json)
+                        return
+                    }
                 }
             }
-        }
-        showError()
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
     }
-
+    
     func parse(json: JSON) {
         for result in json["results"].arrayValue {
             let title = result["title"].stringValue
@@ -41,12 +56,13 @@ class ViewController: UITableViewController {
             let obj = ["title": title, "body": body, "sigs":sigs]
             petitions.append(obj)
         }
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
     }
     
     //MARK: UITableView
